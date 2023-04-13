9ca9f4888991e39a3e0160ac996aa43abe9808cf
Thiago Silva de Oliveira <121137873+ThiagoPy2023@users.noreply.github.com>
2023-04-12 23:00:11 -0300

    Add files via upload

---
+++ App.java
@@ -0,0 +1,23 @@
+
+package EstruturaCondicionalSe;
+
+public class App {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Olá");
+
+        int media = 10;
+
+        if (media >= 7) {
+            if (media == 10) {
+                System.out.println("Parábens, aprovado com excelência");
+            } else {
+                System.out.println("Aprovado");
+            }
+        } else {
+            System.out.println("Reprovado");
+        }
+    }
+
+}
